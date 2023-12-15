061efecbb204869d5066adcdf0a041d1885a264f
Apoorva Rao <apoorvarao147@gmail.com>
2023-12-14 19:34:23 -0500

    local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 //@ts-nocheck
 import "./styles/app.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [cart, setCart] = useState<any>([]);
   const [cartQuantity, setCartQuantity] = useState(0);
+
+  useEffect(() => {
+    let json = localStorage.getItem("cart");
+    const savedCart = JSON.parse(json);
+
+    let qtyjson = localStorage.getItem('quantity');
+    qtyjson = JSON.parse(qtyjson);
+
+    if (savedCart) {
+      setCart(savedCart);
+      setCartQuantity(qtyjson);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(cart);
+    localStorage.setItem("cart", json);
+
+    const qtyjson = JSON.stringify(cartQuantity);
+    localStorage.setItem("quantity", qtyjson);
+  }, [cart, cartQuantity]);
 
   return (
     <div className="App">
