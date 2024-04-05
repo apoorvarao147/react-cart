770a8d6da0bf9558aa06549e339ec2e7fcc00aa1
Apoorva Rao <apoorvarao147@gmail.com>
2024-04-05 12:18:29 -0400

    search bar media

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -8,7 +8,7 @@ import { FaSearch } from "react-icons/fa";
 
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const [filteredProducts, setFilteredProducts] = useState(products);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [option, setOption] = useState();
 
   const handleSearch = (e) => {
@@ -19,10 +19,13 @@ const Products = () => {
     setFilteredProducts(filter);
   };
 
-  const handleOrder = (e) => {
+  const handleSort = (e) => {
     setOption(e.target.value);
+    if (e.target.value === "default") {
+      setFilteredProducts(products);
+    }
     if (e.target.value === "price_asc") {
-      const sortedProducts = products.sort((p1, p2) =>
+      const sortedProducts = filteredProducts.sort((p1, p2) =>
         Number(p1.price) < Number(p2.price)
           ? -1
           : Number(p1.price) > Number(p2.price)
@@ -32,7 +35,7 @@ const Products = () => {
       setFilteredProducts(sortedProducts);
     }
     if (e.target.value === "price_desc") {
-      const sortedProducts = products.sort((p1, p2) =>
+      const sortedProducts = filteredProducts.sort((p1, p2) =>
         Number(p1.price) < Number(p2.price)
           ? 1
           : Number(p1.price) > Number(p2.price)
@@ -42,7 +45,7 @@ const Products = () => {
       setFilteredProducts(sortedProducts);
     }
     if (e.target.value === "title_asc") {
-      const sortedProducts = products.sort((p1, p2) =>
+      const sortedProducts = filteredProducts.sort((p1, p2) =>
         p1.productName.toUpperCase() < p2.productName.toUpperCase()
           ? -1
           : p1.productName.toUpperCase() > p2.productName.toUpperCase()
@@ -52,7 +55,7 @@ const Products = () => {
       setFilteredProducts(sortedProducts);
     }
     if (e.target.value === "title_desc") {
-      const sortedProducts = products.sort((p1, p2) =>
+      const sortedProducts = filteredProducts.sort((p1, p2) =>
         p1.productName.toUpperCase() < p2.productName.toUpperCase()
           ? 1
           : p1.productName.toUpperCase() > p2.productName.toUpperCase()
@@ -69,32 +72,32 @@ const Products = () => {
         `${process.env.REACT_APP_BACKEND_API}/product`
       );
       const data = (await response.json()) as Product[];
-      setProducts(data);
-      setFilteredProducts(data);
+      setProducts([...data]);
+      setFilteredProducts([...data]);
     };
     fetchData();
   }, []);
 
   return (
     <>
+      <div className="products">
       <div className="search">
         <button>
           <input type="text" placeholder="Search" onChange={handleSearch} />
           <FaSearch className="search-icon" />
         </button>
 
-        <label>Sort By</label>
-        <select onChange={handleOrder} defaultValue="newest" value={option}>
-          <option value="newest">Newest</option>
+        <label>Sort By:</label>
+        <select onChange={handleSort} value={option}>
+          <option value="default">Default</option>
           <option value="price_asc">Price: Low to High</option>
           <option value="price_desc">Price: High to Low</option>
           <option value="title_asc">Title: A to Z</option>
           <option value="title_desc">Title: Z to A</option>
         </select>
       </div>
-      <div className="products-main">
         {filteredProducts.map((product) => (
-          <div key={product.id} className="products">
+          <div key={product.id} className="product">
             <div className="product-image">
               <img src={product.url} alt={product.productName} />
             </div>
